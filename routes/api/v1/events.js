543148667a523c4c60e204a71d3de7f2d1f660e6
Tyler Schaffer <tpschaffer@gmail.com>
2020-01-14 14:50:00 -0700

    Build customization for events endpoint to match requirements

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -5,8 +5,21 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
+async function getAllEventsBySport() {
+  var allSports = await database('events').select('sport').distinct()
+  var finalArray = await Promise.all(allSports.map(async (sport_hash) => {
+    var events = await database('events').where({sport: sport_hash.sport}).select('event').distinct()
+    var eventsArray = await Promise.all(events.map(async (event_hash) => {
+      return event_hash.event
+    }))
+    return {'sport': sport_hash.sport, 'events': eventsArray}
+  }))
+  return finalArray
+}
+
 const getAllEvents = router.get('/', async (request, response) => {
-  response.status(200).send({'events': []});
+  const data = await getAllEventsBySport()
+  response.status(200).send({'events': data});
 })
 
 module.exports = {
