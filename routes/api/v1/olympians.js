991c6aaa9c7125b830798817378d1be299ffe45a
Tyler Schaffer <tpschaffer@gmail.com>
2020-01-14 10:20:25 -0700

    Add additional logic to distinguish youngest and oldest queries

--- routes/api/v1/olympians.js
+++ routes/api/v1/olympians.js
@@ -6,11 +6,19 @@ const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
 const getAllOlympians = router.get('/', async (request, response) => {
+  if (request.query.age === 'youngest') {
+    additionalQuery = 'ORDER BY olympians.age ASC LIMIT 1'
+  } else if (request.query.age === 'oldest') {
+    additionalQuery = 'ORDER BY olympians.age DESC LIMIT 1'
+  } else {
+    additionalQuery = ''
+  }
   const olympians = await database.raw('SELECT name, team, age, sport, ' +
     'CAST(COUNT(CASE WHEN medal IS NOT NULL THEN 1 END) AS int) ' +
     'AS total_medals_won FROM olympians ' +
     'INNER JOIN olympian_events ON olympians.id = olympian_events.olympian_id ' +
-    'GROUP BY olympians.name, olympians.team, olympians.age, olympians.sport');
+    'GROUP BY olympians.name, olympians.team, olympians.age, olympians.sport ' +
+    additionalQuery);
   response.status(200).send({'olympians': olympians['rows']});
 })
 
