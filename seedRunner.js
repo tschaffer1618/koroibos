10c2768e53fbf3828ef466e85f98c307d5f7194d
Tyler Schaffer <tpschaffer@gmail.com>
2020-01-13 19:37:27 -0700

    Update runner file to seed events table

--- seedRunner.js
+++ seedRunner.js
@@ -5,11 +5,12 @@ const database = require('knex')(configuration);
 const csv = require('csvtojson');
 
 const Olympian = require('./models/olympian')
+const Event = require('./models/event')
 
 async function cleanTables() {
   // await database.raw('TRUNCATE TABLE olympian_events CASCADE');
   await database.raw('TRUNCATE TABLE olympians CASCADE');
-  // await database.raw('TRUNCATE TABLE events CASCADE');
+  await database.raw('TRUNCATE TABLE events CASCADE');
   console.log('Tables have been reset');
 }
 
@@ -24,9 +25,16 @@ async function createOlympian(row) {
   }
 }
 
-// async function createEvent(row) {
-//
-// }
+async function createEvent(row) {
+  let event = new Event(row);
+  let existingEvent = await database('events').where({event: row.Event});
+  if (existingEvent[0]) {
+    return existingEvent[0]
+  } else {
+    let newEvent = await database('events').insert(event).returning('*');
+    return newEvent[0];
+  }
+}
 
 // async function createOlympianEvent(row) {
 //
@@ -37,6 +45,7 @@ async function seedTables(file) {
     .fromFile(file)
     .subscribe(async (row) => {
       var olympian = await createOlympian(row);
+      var event = await createEvent(row);
     })
   console.log('Tables have been seeded')
 }
