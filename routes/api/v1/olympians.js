ac52cc0bd8b31ce93b95447ac6dd7d9c878b15af
Tyler Schaffer <tpschaffer@gmail.com>
2020-01-14 10:10:53 -0700

    Write query to return total_medals_won for each olympian

--- routes/api/v1/olympians.js
+++ routes/api/v1/olympians.js
@@ -6,8 +6,12 @@ const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
 const getAllOlympians = router.get('/', async (request, response) => {
-  const olympians = await database('olympians').select('name', 'team', 'age', 'sport');
-  response.status(200).send(olympians);
+  const olympians = await database.raw('SELECT name, team, age, sport, ' +
+    'CAST(COUNT(CASE WHEN medal IS NOT NULL THEN 1 END) AS int) ' +
+    'AS total_medals_won FROM olympians ' +
+    'INNER JOIN olympian_events ON olympians.id = olympian_events.olympian_id ' +
+    'GROUP BY olympians.name, olympians.team, olympians.age, olympians.sport');
+  response.status(200).send({'olympians': olympians['rows']});
 })
 
 module.exports = {
